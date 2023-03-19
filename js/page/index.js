88e2bddaecf9a2cf27201c5ed7084513c36db44b
lionel-link <lioneladje@gmail.com>
2023-03-19 13:12:12 +0100

    create not native main input reseacrh

--- js/page/index.js
+++ js/page/index.js
@@ -20,5 +20,73 @@ function buidRecipesDom(recipesDom) {
 }
 
 
+<<<<<<< Updated upstream
+=======
+  if (searchBar.value.length >= 3) {
+    let length = searchBar.value.length;
+
+    recipes.forEach((recipe) => {
+      let SlitedTitle = recipe.name.split(" ");
+      SlitedTitle.forEach((word) => {
+        if (word.substring(0, length).toUpperCase() === searchBar.value.toUpperCase()) {
+          if (recipesList.length === 0) {
+            recipesList.push(recipe);
+          } else {
+            recipesList.forEach((recip) => {
+              console.log(!(recip === recipe));
+              if (!(recip === recipe)) {
+                recipesList.push(recipe);
+              }
+            });
+          }
+        }
+      });
+
+      let SlitedDescription = recipe.name.split(" ");
+      SlitedDescription.forEach((word) => {
+        if (word.substring(0, length).toUpperCase() === searchBar.value.toUpperCase()) {
+          if (recipesList.length === 0) {
+            recipesList.push(recipe);
+          } else {
+            recipesList.forEach((recip) => {
+              console.log(!(recip === recipe));
+              if (!(recip === recipe)) {
+                recipesList.push(recipe);
+              }
+            });
+          }
+        }
+      });
+
+      recipe.ingredients.forEach((ingredient) => {
+        let SlitedIngredient = ingredient.ingredient.split(" ");
+        SlitedIngredient.forEach((word) => {
+          if (word.substring(0, length).toUpperCase() === searchBar.value.toUpperCase()) {
+            if (recipesList.length === 0) {
+              recipesList.push(recipe);
+            } else {
+              recipesList.forEach((recip) => {
+                console.log(!(recip === recipe));
+                if (!(recip === recipe)) {
+                  recipesList.push(recipe);
+                }
+              });
+            }
+          }
+        });
+      });
+    });
+    refresh = true;
+    buidRecipesDom(recipesList);
+    return;
+  }
+
+  if (refresh && searchBar.value.length < 3) {
+    cardContainer.removeChild(cardContainer.children[0]);
+    buidRecipesDom();
+    refresh = false;
+  }
+}
+>>>>>>> Stashed changes
 
 buidRecipesDom();
