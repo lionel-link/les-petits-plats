6edd8942c2a73260a441624ac0c2a39d8b6f3437
lionel-link <lioneladje@gmail.com>
2023-03-24 08:43:09 +0100

    refact code session

--- js/page/index.js
+++ js/page/index.js
@@ -3,9 +3,104 @@ import { recipesFactory } from "../factories/recipesFactories.js";
 
 const cardContainer = document.getElementById("card-container");
 const searchBar = document.getElementById("main-search-input");
-searchBar.addEventListener("keyup", filter);
+searchBar.addEventListener("keyup", ()=> {
+  const value = searchBar.value;
+  filter(value);
+});
 
-const recipes = getRecipes();
+const recipes = [
+  {
+    id: 40,
+    name: "Limonade",
+    servings: 4,
+    ingredients: [
+      {
+        ingredient: "Eau",
+        quantity: 1,
+        unit: "Litres",
+      },
+      {
+        ingredient: "Citron Vert",
+        quantity: 3,
+      },
+      {
+        ingredient: "Sucre en poudre",
+        quantity: 4,
+        unit: "cuillères à café",
+      },
+      {
+        ingredient: "Bicarbonate",
+        quantity: 1,
+        unit: "cuillères à café",
+      },
+    ],
+    time: 10,
+    description:
+      "Dans un saladier mettre l'eau, le jus des citrons et le sucre. Bien mélanger. Ajouter le bicarbonate. Servir. Ajouter des glaçon et une feuille de menthe pour la déco.",
+    appliance: "Saladier",
+    ustensils: ["cuillère en bois"],
+  },
+  {
+    id: 41,
+    name: "Mousse au chocolat",
+    servings: 4,
+    ingredients: [
+      {
+        ingredient: "Oeuf",
+        quantity: 3,
+      },
+      {
+        ingredient: "Chocolat noir",
+        quantity: 100,
+        unit: "grammes",
+      },
+      {
+        ingredient: "Sucre vanillé",
+        quantity: 1,
+        unit: "sachets",
+      },
+    ],
+    time: 20,
+    description:
+      "Séparer les blancs d'oeufs. Faire fondre le chocolat au bain marie. Ajouter les jaunes et le sucre au chocolat hors du feu. Battre les blancs en neige. Ajouter les blancs au mélange de chocolat. Mélangez délicatement avec une spatule. Servir dans un plat ou dans des verres. Mettre au frais",
+    appliance: "Casserole",
+    ustensils: ["fouet", "spatule", "verres"],
+  },
+  {
+    id: 1,
+    name: "Limonade de Coco",
+    servings: 1,
+    ingredients: [
+      {
+        ingredient: "Lait de coco",
+        quantity: 400,
+        unit: "ml",
+      },
+      {
+        ingredient: "Jus de citron",
+        quantity: 2,
+      },
+      {
+        ingredient: "Crème de coco",
+        quantity: 2,
+        unit: "cuillères à soupe",
+      },
+      {
+        ingredient: "Sucre",
+        quantity: 30,
+        unit: "grammes",
+      },
+      {
+        ingredient: "Glaçons",
+      },
+    ],
+    time: 10,
+    description:
+      "Mettre les glaçons à votre goût dans le blender, ajouter le lait, la crème de coco, le jus de 2 citrons et le sucre. Mixer jusqu'à avoir la consistence désirée",
+    appliance: "Blender",
+    ustensils: ["cuillère à Soupe", "verres", "presse citron"],
+  },
+]
 const ingredients = getIngredients();
 const appliances = getAppliances();
 const ustensils = getUstensils();
@@ -33,21 +128,24 @@ function buidRecipesDom(recipesDom) {
   }
 }
 
-function filter(research) {
+function filter(value) {
+  value = value.toUpperCase();
   let recipesList = [];
+  let length = value.length;
 
-  if (searchBar.value.length >= 3) {
-    let length = searchBar.value.length;
+  if (length >= 3) {
+    
 
     recipes.forEach((recipe) => {
       let SlitedTitle = recipe.name.split(" ");
       SlitedTitle.forEach((word) => {
-        if (word.substring(0, length).toUpperCase() === searchBar.value.toUpperCase()) {
+        if (word.substring(0, length).toUpperCase() === value) {
           if (recipesList.length === 0) {
             recipesList.push(recipe);
           } else {
             recipesList.forEach((recip) => {
-              if (!(recip === recipe)) {
+              console.log(recip.id , recipe.id, recip.id != recipe.id);
+              if (recip.id != recipe.id) {
                 recipesList.push(recipe);
               }
             });
@@ -57,12 +155,12 @@ function filter(research) {
 
       let SlitedDescription = recipe.name.split(" ");
       SlitedDescription.forEach((word) => {
-        if (word.substring(0, length).toUpperCase() === searchBar.value.toUpperCase()) {
+        if (word.substring(0, length).toUpperCase() === value) {
           if (recipesList.length === 0) {
             recipesList.push(recipe);
           } else {
             recipesList.forEach((recip) => {
-              if (!(recip === recipe)) {
+              if (recip.id != recipe.id) {
                 recipesList.push(recipe);
               }
             });
@@ -73,12 +171,12 @@ function filter(research) {
       recipe.ingredients.forEach((ingredient) => {
         let SlitedIngredient = ingredient.ingredient.split(" ");
         SlitedIngredient.forEach((word) => {
-          if (word.substring(0, length).toUpperCase() === searchBar.value.toUpperCase()) {
+          if (word.substring(0, length).toUpperCase() === value) {
             if (recipesList.length === 0) {
               recipesList.push(recipe);
             } else {
               recipesList.forEach((recip) => {
-                if (!(recip === recipe)) {
+                if (recip.id != recipe.id) {
                   recipesList.push(recipe);
                 }
               });
